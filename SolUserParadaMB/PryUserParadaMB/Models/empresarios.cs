7d209f6f98cf1ac248a17c0daa95be8a3abeefb9
Personal <Personal@PERSONAL>
2021-01-25 20:46:36 -0400

    parte del pograma

--- SolUserParadaMB/PryUserParadaMB/Models/empresarios.cs
+++ SolUserParadaMB/PryUserParadaMB/Models/empresarios.cs
@@ -1,11 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿
 
 namespace PryUserParadaMB.Models
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
     public class empresarios
     {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Nombre Completo", MinimumLength = 5)]
+
+        public string Name { get; set; }
+        [EmailAddress]
+        [StringLength(100, ErrorMessage = "ingrese su Email", MinimumLength = 5)]
+        public string Email { get; set; }
+
+        
+
+        public string street { get; set; }
+
+        [StringLength(100, ErrorMessage = "ingrese su telefono", MinimumLength = 5)]
+
+        public int phone { get; set; }
+
+        [StringLength(100, ErrorMessage = "ingrese el website", MinimumLength = 5)]
+        public string website { get; set; }
+
+
+
+
+
+
+
+
+
+
     }
+    public class Geo
+    {
+        public double lat { get; set; }
+
+        public double ing { get; set; }
+    }
+   
+
 }
